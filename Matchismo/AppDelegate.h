b230f39ee6933af0ffb12606da6fa6d2345cf7d7
Dudu Lacerda <dudu.lacerda@gmail.com>
2013-07-10 10:16:25 -0300

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Eduardo Lacerda on 10/07/13.
+//  Copyright (c) 2013 Eduardo Lacerda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Eduardo Lacerda on 10/07/13.
+//  Copyright (c) 2013 Eduardo Lacerda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
